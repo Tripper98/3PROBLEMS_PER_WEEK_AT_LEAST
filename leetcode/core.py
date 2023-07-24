21d04f07749e100d5c068bc2829ab2eabc3b6f77
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2023-07-25 00:25:36 +0200

    solution: add solution for romantoint for some cases

--- leetcode/core.py
+++ leetcode/core.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Solution(object):
     def romanToInt(self, s):
         """
@@ -13,16 +15,29 @@ class Solution(object):
             'D': 500,
             'M': 1000
         }
-        converted_number = dict_roman[s[0]]
-        len_s = len(s)
-        tmp = 0 ; i = 0
-        while i != len_s-1:
-        # for i in range(1, len_s-1):
-            tmp = dict_roman[s[i]]
-            if dict_roman[s[i+1]] <= tmp:
-                converted_number += (tmp+dict_roman[s[i+1]])
-            else:
-                converted_number -= (dict_roman[s[i+1]]-tmp)
-            i += 2
+        converted_number = 0
+        i = 0 ; cmp = len(s)
 
+        while cmp >=1:
+            if dict_roman[s[i]] >= dict_roman[s[i+1]]:
+                converted_number +=dict_roman[s[i]]
+                i+=1;cmp-=1
+            else:
+                converted_number += (dict_roman[s[i+1]]- dict_roman[s[i]])
+                i+=2;cmp -=2
+            print(converted_number)
+        # converted_number += dict_roman[s[-1]]
         return converted_number
+    
+
+class BasicTestSuite(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_fproblem(self):
+        x = Solution().romanToInt(s= "LVIII")
+        print(x)
+        assert x == 58, "Should be 58"
+
+
+if __name__ == '__main__':
+    unittest.main()
