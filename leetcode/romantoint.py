06e9aa30a7174a365e940812787b118508e1e870
DRIOUCHE Adnane <adnanedriouche98@gmail.com>
2023-07-26 00:02:37 +0200

    fix: testing seperatly still doesn't work

--- leetcode/romantoint.py
+++ leetcode/romantoint.py
@@ -1,4 +1,3 @@
-import unittest
 
 class Solution(object):
     def romanToInt(self, s):
@@ -36,14 +35,3 @@ class Solution(object):
         return converted_number
     
 
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
-
-    def test_fproblem(self):
-        x = Solution().romanToInt(s= "MCMXCIV")
-        print(x)
-        assert x == 1994, "Should be 58"
-
-
-if __name__ == '__main__':
-    unittest.main()
